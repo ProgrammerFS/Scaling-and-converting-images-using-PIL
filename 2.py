255ca3cc2065668a4c451d864ac3b1ca30b3a6c1
ProgrammerFS <69897389+ProgrammerFS@users.noreply.github.com>
2022-09-16 19:36:47 +0400

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-def image_operator(image_dir, degrees, new_size, target_dir):
-    for i in os.listdir(image_dir):
-        im = Image.open(f"{image_dir}/"+i)
-        new_im = im.rotate(degrees).resize(new_size).convert("RGB")
-        new_im.save(f"{target_dir}/"+i, "jpeg")
-
-image_operator("./images", 90, (128,128), "./opt/icons")
