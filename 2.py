48cbdba311c9baf6d7ee6c9381600ef8e6c8396c
Mohammed Fahad <programtesting205@gmail.com>
2022-09-16 19:30:21 +0400

    revised 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,9 @@
 import os
 from PIL import Image
-for i in os.listdir("./images"):
-    im = Image.open("./images/"+i)
-    new_im = im.rotate(90).resize((128,128))
-    new_im.save("./opt/icons/"+i, "jpeg")
+def image_operator(image_dir, degrees, new_size, target_dir):
+    for i in os.listdir(image_dir):
+        im = Image.open(f"{image_dir}/"+i)
+        new_im = im.rotate(degrees).resize(new_size).convert("RGB")
+        new_im.save(f"{target_dir}/"+i, "jpeg")
+
+image_operator("./images", 90, (128,128), "./opt/icons")
